0e1e91f65e5517fbe2d0c28bec9b6f04f93af93d
Steve Burton and Suzi Rubino <steve@>
2016-10-25 17:57:08 -0700

    fix for loops so sentence displays properly

--- js/scripts.js
+++ js/scripts.js
@@ -5,33 +5,28 @@ $(document).ready(function() {
     var sentenceSplit = userSentence.split("");
     var mixSentence = [];
 
-
-
     for (var count = 0; count < sentenceSplit.length; count += 1) {
-      if (sentenceSplit[count] === "a") {
-        mixSentence.push("-");
-      }
-
+      var mixSentence = sentenceSplit.map(function(count, letter) {
+        if (count === "a") {
+          return "-";
+        } else if (count === "e") {
+          return "-";
+        } else if (count === "i") {
+          return "-";
+        } else if (count === "o") {
+          return "-";
+        } else if (count === "u") {
+          return "-";
+        } else if (count === "y") {
+          return "-";
+        } else {
+          return count;
+        }
+      });
     }
-console.log(sentenceSplit);
-
-    //
-    // for ( var count = 0; count < items.length; count += 1) {
-    //
-    // groceryList.push(items[count]);
-    //
-    // }
-    //
-    // $("#display").text(groceryList);
+    var puzzleSentence = mixSentence.join("");
+    $("#output").text(puzzleSentence);
 
     event.preventDefault();
-
-
   });
 });
-
-
-
-    // var userSentence = [$("input#sentence").val()];
-    // console.log(userSentence);
-    // var vowels = ["aeiouy"];
